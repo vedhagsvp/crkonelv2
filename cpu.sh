083da88ae0d4558c1e68d707d01205b2442f532c
vedhagsvp <vedhag230223@gmail.com>
2025-03-07 00:01:21 +0530

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-./kormnr -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.$RANDOM@as.catchthatrabbit.com:8008 &&
-./kormnr -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.$RANDOM@eu.catchthatrabbit.com:8008 &&
-./kormnr -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.$RANDOM@us.catchthatrabbit.com:8008
+./kormnr -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.VI2000@eu.catchthatrabbit.com:8008 &&
+./kormnr -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.VI2000@as.catchthatrabbit.com:8008 &&
+./kormnr -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.VI12000@us.catchthatrabbit.com:8008 && 
+./kormnr -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.VI12000@eu1.catchthatrabbit.com:8008 &&
+./kormnr -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.VI12000@as1.catchthatrabbit.com:8008 &&
+./kormnr -P stratum://cb16e28eda52f3750022c539e75c84610f87af8030a9.VI12000@us1.catchthatrabbit.com:8008
